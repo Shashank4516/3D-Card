e70e9e63809dcac4c2f6da508bfd10c9048adea2
Shashank <102940527+Shashank4516@users.noreply.github.com>
2024-09-13 22:22:06 +0530

    Add files via upload

---
+++ 3d-card.js
@@ -0,0 +1,26 @@
+const header = document.getElementById("header");
+const title = document.getElementById("title");
+const excerpt = document.getElementById("excerpt");
+const profile_img = document.getElementById("profile_img");
+const name = document.getElementById("name");
+const date = document.getElementById("date");
+
+const animated_bgs = document.querySelectorAll(".animated-bg");
+const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
+
+setTimeout(getData, 2500);
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1559494007-9f5847c49d94?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />';
+  title.innerHTML = "Seaside Escapes: Discover the Beauty of Coastal Paradise";
+  excerpt.innerHTML =
+    "Seaside Escapes offers a perfect blend of relaxation and adventure, inviting travelers to explore pristine beaches, crystal-clear waters, and breathtaking .";
+  profile_img.innerHTML =
+    '<img src="https://images.unsplash.com/photo-1499996860823-5214fcc65f8f?q=80&w=1966&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />';
+  name.innerHTML = "Shashank";
+  date.innerHTML = "20/04/2024";
+
+  animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
+  animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
+}
